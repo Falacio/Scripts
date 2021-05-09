37e8af49fb430d677e685f4a02efb5028fa3da80
Falacio <37366382+Falacio@users.noreply.github.com>
2021-05-09 16:25:53 +0200

    Add files via upload

---
+++ Resumen_log.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  9 13:54:59 2021
+
+@author: Falacio
+"""
+
+from getpass import getuser
+usuario = getuser()
+
+ruta_full = 'C:/Users/'+ usuario +'/.chia/mainnet/log/debug.log'
+ruta_farm = 'C:/Users/'+ usuario +'/.chia/mainnet/log/farm_debug.log'
+with open(ruta_full) as full_log, open(ruta_farm, 'w') as farm_log:
+    for linea in full_log:
+        if 'harvester' in linea or 'farmer' in linea:
+            farm_log.write(linea)
